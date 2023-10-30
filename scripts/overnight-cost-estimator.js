bb70f2123b144a8c655ed8665c99db7e19d7305b
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-29 20:02:21 -0400

    add output for the overnight estimator

--- scripts/overnight-cost-estimator.js
+++ scripts/overnight-cost-estimator.js
@@ -8,7 +8,32 @@ function init() {
 }
 
 function estimateStayCost() {
-    const basicNightRate = 150;
+    // get the checkInDate
+    const checkInDateInput = document.querySelector('#checkInDate').value;
+    // convert checkInDate to a Date 
+    const checkInDate = new Date (checkInDateInput);
+    // get the month of the check in date 
+    const checkInMonth = checkInDate.getMonth() + 1;
+
+    let nightRate = 0;
+    const queen = document.querySelector('#queen');
+    const king = document.querySelector('#king');
+
+     // summer month rates
+    if (checkInMonth >= 6 && checkInMonth <= 8) {
+        if (queen.checked || king.checked) {
+            nightRate = 250;
+        } else {
+            nightRate = 350;
+        }
+    // non summer month rates
+    } else {
+        if (queen.checked || king.checked) {
+            nightRate = 150;
+        } else {
+            nightRate = 210;
+        }
+    }
 
     // apply discount
     let discount = 0; 
@@ -16,16 +41,38 @@ function estimateStayCost() {
     const military = document.querySelector('#military');
 
     if (AAA.checked) {
-        discount = (10/100);
+        discount = (10);
     } else if (military.checked) {
-        discount = (20/100);
+        discount = (20);
     } else {
         discount = 0;
     }
 
-    const discountedRoomCost = basicNightRate - (discount * basicNightRate);
+    const discountedRoomCost = nightRate - ((discount/100) * nightRate);
 
     // apply taxes
     let taxes = (12/100) * discountedRoomCost;
+
+    //sum of everything 
+
+    // get the number of nights stayed
+    const nightsStayed = document.querySelector('#numberOfNights').value;
+    const totalCost = (discountedRoomCost * nightsStayed) + taxes;
+
+    //outputs 
+    const originalRoomCostOutput = document.querySelector('#originalRoomCost');
+    originalRoomCostOutput.value = nightRate.toFixed(2);
+
+    const discountAppliedOutput = document.querySelector('#discountApplied');
+    discountApplied.value = `${discount.toFixed(2)} %`;
+
+    const discountedRoomCostOutput = document.querySelector('#discountedRoomCost');
+    discountedRoomCostOutput.value = discountedRoomCost.toFixed(2);
+
+    const taxOutput = document.querySelector('#tax');
+    taxOutput.value = taxes.toFixed(2);
+
+    const totalCostOutput =  document.querySelector('#totalCost');
+    totalCostOutput.value = totalCost.toFixed(2);
 }
 
